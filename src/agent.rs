6c57ab37f0c25d0259e0479551e9f7095a8d2636
Jesus Guzman, Jr <jesus@marzipan.club>
2023-12-28 23:37:59 -0700

    add logging to unit tests

--- src/agent.rs
+++ src/agent.rs
@@ -10,6 +10,7 @@ use {
 const DEFAULT_GRACE_PERIOD: Duration = Duration::from_secs(3);
 
 /// Error returned when trying to send a message to an agent that has been terminated.
+/// Returns the message that couldn't be sent.
 #[derive(thiserror::Error, Debug, PartialEq, Eq, Clone, Copy)]
 #[error("unable to send message to terminated agent")]
 pub struct SendError<M>(pub M);
@@ -21,6 +22,7 @@ pub struct Sender<M>(UnboundedSender<M>);
 impl<M> Sender<M> {
     /// Send a message to the agent.
     pub fn send(&self, message: M) -> Result<(), SendError<M>> {
+        // map the tokio SendError to our own SendError
         self.0.send(message).map_err(|m| SendError(m.0))
     }
 }
@@ -103,5 +105,62 @@ where
     /// Returns a sender that can be used to send messages to the agent.
     pub fn sender(&self) -> Sender<M> {
         Sender(self.sender.clone())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    type TokioSendError<T> = tokio::sync::mpsc::error::SendError<T>;
+    type Error<T> = SendError<T>;
+
+    fn agent_id(id: usize) -> String {
+        format!("agent {}", id)
+    }
+
+    #[tokio::test]
+    async fn test_actor_processes_message() -> Result<(), Error<&'static str>> {
+        let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
+
+        let agent = Agent::new(agent_id(1), move |message| {
+            let tx = tx.clone();
+            async move {
+                tx.send(message)?;
+                Result::<_, TokioSendError<_>>::Ok(())
+            }
+        });
+
+        let message = "hello world";
+        agent.send(message)?;
+        assert_eq!(rx.recv().await, Some(message));
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_multiple_agents() -> Result<(), Error<&'static str>> {
+        let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
+
+        let agent_1 = Agent::new(agent_id(1), move |message| {
+            let tx = tx.clone();
+            async move {
+                tx.send(message)?;
+                Result::<_, TokioSendError<_>>::Ok(())
+            }
+        })
+        .sender();
+
+        let agent_2 = Agent::new(agent_id(2), move |message| {
+            let agent_1 = agent_1.clone();
+            async move {
+                agent_1.send(message)?;
+                Result::<_, Error<&'static str>>::Ok(())
+            }
+        });
+
+        let message = "hello world";
+        agent_2.send(message)?;
+        assert_eq!(rx.recv().await, Some(message));
+        Ok(())
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,58 +3,17 @@ pub mod agent;
 pub use agent::Agent;
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+#[ctor::ctor]
+fn init() {
+    use tracing_subscriber::layer::SubscriberExt;
+    use tracing_subscriber::util::SubscriberInitExt;
 
-    type TokioSendError<T> = tokio::sync::mpsc::error::SendError<T>;
-    type Error<T> = agent::SendError<T>;
-
-    fn agent_id(id: usize) -> String {
-        format!("agent {}", id)
-    }
-
-    #[tokio::test]
-    async fn test_actor_processes_message() -> Result<(), Error<&'static str>> {
-        let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
-
-        let agent = Agent::new(agent_id(1), move |message| {
-            let tx = tx.clone();
-            async move {
-                tx.send(message)?;
-                Result::<_, TokioSendError<_>>::Ok(())
-            }
-        });
-
-        let message = "hello world";
-        agent.send(message)?;
-        assert_eq!(rx.recv().await, Some(message));
-        Ok(())
-    }
-
-    #[tokio::test]
-    async fn test_multiple_agents() -> Result<(), Error<&'static str>> {
-        let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
-
-        let agent_1 = Agent::new(agent_id(1), move |message| {
-            let tx = tx.clone();
-            async move {
-                tx.send(message)?;
-                Result::<_, TokioSendError<_>>::Ok(())
-            }
-        })
-        .sender();
-
-        let agent_2 = Agent::new(agent_id(2), move |message| {
-            let agent_1 = agent_1.clone();
-            async move {
-                agent_1.send(message)?;
-                Result::<_, Error<&'static str>>::Ok(())
-            }
-        });
-
-        let message = "hello world";
-        agent_2.send(message)?;
-        assert_eq!(rx.recv().await, Some(message));
-        Ok(())
-    }
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::fmt::layer()
+                .without_time()
+                .with_line_number(true),
+        )
+        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
 }
